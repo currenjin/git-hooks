e5ecabda88f83a747106f6a0604cc6800c0b5c73
currenjin <hyun0524e@naver.com>
2024-02-01 12:21:42 +0900

    Add: init project

---
+++ ktlint-spring/src/test/kotlin/com/currenjin/ktlintspring/KtlintSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.currenjin.ktlintspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtlintSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
