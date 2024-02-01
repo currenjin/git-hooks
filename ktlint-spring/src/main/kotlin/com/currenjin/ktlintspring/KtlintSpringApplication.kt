995ce94b71b89cd0b065b577a6c757d146f04588
currenjin <hyun0524e@naver.com>
2024-02-01 12:37:59 +0900

    Wip: Break lint

--- ktlint-spring/src/main/kotlin/com/currenjin/ktlintspring/KtlintSpringApplication.kt
+++ ktlint-spring/src/main/kotlin/com/currenjin/ktlintspring/KtlintSpringApplication.kt
@@ -6,6 +6,5 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class KtlintSpringApplication
 
-fun main(args: Array<String>) {
-    runApplication<KtlintSpringApplication>(*args)
+fun main(args: Array<String>) { runApplication<KtlintSpringApplication>(*args)
 }
